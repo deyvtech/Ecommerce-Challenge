96708f992e3c1cb68a3556a786297cfd40f2cd21
Geeko24 <kingnorway17@gmail.com>
2022-09-23 13:19:53 +0800

    Add add and remove img button

--- main.js
+++ main.js
@@ -1,20 +1,41 @@
-let a = 0
+// Add and Minus
+let a = 0;
+document.querySelector(".plus").addEventListener("click", () => {
+  a++;
+  document.querySelector(".num").innerText = a;
+});
 
+document.querySelector(".minus").addEventListener("click", () => {
+  if (a >= 1) {
+    a--;
+    document.querySelector(".num").innerText = a;
+  }
+});
 
+// const btnImgs = document.querySelectorAll('.small-row-img')
+// btnImgs.forEach((btnImg)=>{
+//     btnImg.addEventListener('click', (e) => {
+//         btnImg.classList.toggle('active')
+//     })
+// })
 
+btns(document.querySelectorAll(".small-row-img"))
 
-document.querySelector('.plus').addEventListener('click', () => {
-    a++ 
-    document.querySelector('.num').innerText = a;
+function btns(btn) {
+    for (let i = 0; i < btn.length; i++) {
+        btn[i].addEventListener("click", function(e) {
+        let current = this;
+        for (let i = 0; i < btn.length; i++) {
+          if (current != btn[i]) {
+            btn[i].classList.remove('active');
+          } else if (current.classList.contains('active') === true) {
+            current.classList.remove('active');
+          } else {
+            current.classList.add('active')
+          }
+        }
+        e.preventDefault();
+      });
+    };
+  }
 
-})
-
-document.querySelector('.minus').addEventListener('click', () => {
-    if (a >= 1) {
-        a--
-        document.querySelector('.num').innerText = a;
-    }
-
-   
-
-})
